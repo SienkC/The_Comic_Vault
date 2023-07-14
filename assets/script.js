6fcb1cb8f3e46f3996a6be4fb6d2dfc6ad60f2a6
Felix Francisco <Felixfrancisco1776@gmail.com>
2023-07-14 12:48:10 -0400

    uploading a barebone website

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+// Sample data - replace with your own data
+const data = [
+    { title: 'Comic Book 1', category: 'Comic Books' },
+    { title: 'Comic Book 2', category: 'Comic Books' },
+    { title: 'Movie 1', category: 'Movies' },
+    { title: 'Movie 2', category: 'Movies' }
+  ];
+  
+  // Get references to HTML elements
+  const searchInput = document.getElementById('search-input');
+  const searchButton = document.getElementById('search-button');
+  const searchResults = document.getElementById('search-results');
+  
+  // Function to perform search
+  function performSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredData = data.filter(item => {
+      const title = item.title.toLowerCase();
+      return title.includes(searchTerm);
+    });
+  
+    displayResults(filteredData);
+  }
+  
+  // Function to display search results
+  function displayResults(results) {
+    searchResults.innerHTML = '';
+    if (results.length > 0) {
+      results.forEach(item => {
+        const resultItem = document.createElement('div');
+        resultItem.innerHTML = `${item.title} - ${item.category}`;
+        searchResults.appendChild(resultItem);
+      });
+    } else {
+      const noResults = document.createElement('div');
+      noResults.textContent = 'No results found.';
+      searchResults.appendChild(noResults);
+    }
+  }
+  
+  // Event listener for search button click
+  searchButton.addEventListener('click', performSearch);
+  
