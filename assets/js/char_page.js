48778f4c69d365bbb70b1064f055447ab2c707b7
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-19 01:11:59 -0400

    Fixed issues list in character page so it matches other buttons

--- assets/js/char_page.js
+++ assets/js/char_page.js
@@ -5,6 +5,8 @@ var startUrl = localStorage.getItem('tempUrl');
 var url = "https://floating-headland-95050.herokuapp.com/" + startUrl + "?api_key=871377ac063cfca6e414991a01d6b3fdfce67591&format=json&field_list=first_appeared_in_issue,issue_credits,name,real_name,deck,image";
 
 
+searchResults.textContent = "Results may take a few seconds to load...";
+
 fetch(url).then(function (response) {
     if(response.ok) {
         response.json().then(function(charDetails) {
@@ -44,10 +46,12 @@ fetch(url).then(function (response) {
 
             for(let i = 0; i < issueLimit; i++) {
                 if(!(charDetails.results.issue_credits[i].name == null || charDetails.results.issue_credits[i].name == "")) {
-                    var issueName = document.createElement('li');
+                    var issueNameHolder = document.createElement('li');
+                    var issueName = document.createElement('p')
                     issueName.textContent = charDetails.results.issue_credits[i].name;
 
                     var link = document.createElement('button');
+                    link.setAttribute('class', 'page-button');
 
                     link.addEventListener('click', function() {
                         // add info needed to Google Books API function
@@ -67,7 +71,8 @@ fetch(url).then(function (response) {
                         
                     })
                     link.appendChild(issueName);
-                    issueList.appendChild(link);
+                    issueNameHolder.appendChild(link);
+                    issueList.appendChild(issueNameHolder);
                     // issueList.appendChild(issueName);
                 }
             }
